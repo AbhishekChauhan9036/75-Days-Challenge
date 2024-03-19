b88f8e0c6b2488534011ea0c6658260f9575fcc4
Abhishek Chauhan <abhishekchauhan9036@gmail.com>
2024-03-19 11:05:29 +0530

    Day 1

---
+++ DayOne.java
@@ -0,0 +1,11 @@
+public class DayOne {
+
+    public static void Solve(int n) {
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        Solve(n);
+    }
+}
