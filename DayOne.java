66c5c0a566294e339aaec5d8ec91449e1d220a2a
Abhishek Chauhan <abhishekchauhan9036@gmail.com>
2024-03-19 18:39:36 +0530

    Day 1

--- DayOne.java
+++ DayOne.java
@@ -1,11 +1,42 @@
-public class DayOne {
+public class DayOne{
+    public static void main(String...args) {
+        int a,b;
+        float c,d;
+        c=20f;
+        d=40f;
+        a=10;
+        b=20;
+        System.out.println(TSRS(a,b));
+        TSRN(c,d);
+        System.out.println(TNRS());
+        TNRN();
 
-    public static void Solve(int n) {
-        System.out.println(n);
     }
 
-    public static void main(String[] args) {
-        int n = 10;
-        Solve(n);
+    public static int TSRS(int n,int m){
+        return n+m;
     }
+
+    public static void TSRN(float a,float b){
+        System.out.println(a-b);
+    }
+
+    public static double TNRS(){
+        return 10.008;
+    }
+
+    public static void TNRN(){
+        System.out.println(TNRS());
+    }
+
+
 }
+
+
+/**
+ * FUNCTION NATURE
+ * TSRS - Takes Something Return Something
+ * TSRN - Takes Something Return Nothing
+ * TNRS - Takes Nothing Return Something
+ * TNRN - Takes Nothing Return Nothing
+ */
